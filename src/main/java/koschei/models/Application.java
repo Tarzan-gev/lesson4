e8286e1da03d53dabc8b732739b1f1a88e7450e4
tarzan <gevorgyan.tarzan@mail.ru>
2026-01-05 03:11:17 +0300

    изменение

--- src/main/java/koschei/models/Application.java
+++ src/main/java/koschei/models/Application.java
@@ -1,4 +1,15 @@
 package koschei.models;
 
+import koschei.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Application {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        KoscheiTheDeathless koscheiTheDeathless =
+                applicationContext.getBean(KoscheiTheDeathless.class);
+        System.out.println(koscheiTheDeathless.getRulesByDeth());
+    }
 }
